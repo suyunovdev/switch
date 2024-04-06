62ba417be040f3c069c21d6150e91a894495a2c1
suyunovdev <153995037+suyunovdev@users.noreply.github.com>
2024-04-07 01:58:19 +0500

    finished

---
+++ main.js
@@ -0,0 +1,137 @@
+//  let browser = "edge"
+
+// if (browser == "edge"){
+//     alert("you have got athe Edge");
+
+//   }
+//   else if(browser == "Chrome" || browser == "firefox", browser == "google", browser == "mozilla"){
+//     alert("okey")
+//   }
+//   else{
+//     alert("We hope")
+//   }
+
+
+
+
+
+// let a = +prompt(`'a?',''`);
+
+// switch (a){
+//     case 0:
+//     alert( 0 )
+//     break;
+//     case 1:
+//     alert(1)
+//     break;
+//     case 2:
+//     case 3:
+// alert('2,3')
+// }
+
+
+
+
+
+
+//////////Homework///////////////////
+
+// 1. Berilgan code'ni switch da yozing:
+//   let a = +prompt('a?', '');
+
+//   if (a == 0) {
+//     alert( 0 );
+//   }
+//   if (a == 1) {
+//     alert( 1 );
+//   }
+
+//   if (a == 2 || a == 3) {
+//     alert( '2,3' );
+//   }
+
+        // let a = +prompt('a?', '');
+        // switch(a){
+        //     case 0:
+        // alert(0);
+        // break;
+        // case 1:
+        // alert(1);
+        // break;
+        // case 2:
+        // case 3:
+        // alert ('2,3');
+        // break;
+        // }
+
+
+
+
+
+
+// 2. Agar bool rost bo'lsa, "ha" ni, aks holda "yo'q" ni qaytarish uchun ternary operator(?) dan foydalanadigan funktsiyani yozing:
+
+// yeah_nope(true) ➞ "ha"
+// yeah_nope(false) ➞ "yo'q"
+
+        
+        // function nope (bool){
+        //     return bool? "ha":"yo'q";
+        // } 
+        // console.log(nope(true));
+        // console.log(nope(false));
+
+
+// 3. Argument sifatida raqamni oladigan va juft sonlar uchun “juft”, toq sonlar uchun “toq” qaytaradigan funksiya yarating:
+
+// isEvenOrOdd(3) ➞ "toq"
+// isEvenOrOdd(146) ➞ "juft"
+// isEvenOrOdd(19) ➞ "toq"
+
+    //         function isEvenOrOdd(number) {
+    //     if (number % 2 === 0) {
+    //         return "juft";
+    //     } else {
+    //         return "toq";
+    //     }
+    // }
+    // console.log(isEvenOrOdd(3));   
+    // console.log(isEvenOrOdd(146)); 
+    // console.log(isEvenOrOdd(19));  
+
+
+
+
+
+//     4. Raqamni yagona argument sifatida qabul qiladigan va u noldan kichik yoki teng bo'lsa, true, aks holda false'ni qaytaradigan funksiya yarating:
+
+// lessThanOrEqualToZero(5) ➞ false
+// lessThanOrEqualToZero(0) ➞ true
+// lessThanOrEqualToZero(-2) ➞ true
+
+
+            // function lessThanOrEqualToZero(number) {
+            //     return number <= 0;
+            // }
+            // console.log(lessThanOrEqualToZero(5));  
+            // console.log(lessThanOrEqualToZero(0));  
+            // console.log(lessThanOrEqualToZero(-2)); 
+
+
+
+
+
+
+
+
+
+            
+code:
+    function isSeven(x) {
+        return x === 7 ? true : false;
+    }
+    
+
+
+
+
